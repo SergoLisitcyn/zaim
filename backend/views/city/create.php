f6dc43c6eb053b66b694850511e5f9922b772f11
sergo <sergolisitcyn@gmail.com>
2021-02-09 01:42:41 +0200

    All new

--- backend/views/city/create.php
+++ backend/views/city/create.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\City */
 
-$this->title = 'Create City';
-$this->params['breadcrumbs'][] = ['label' => 'Cities', 'url' => ['index']];
+$this->title = 'Создать город';
+$this->params['breadcrumbs'][] = ['label' => 'Города', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="city-create">
--- backend/views/city/update.php
+++ backend/views/city/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\City */
 
-$this->title = 'Update City: ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Cities', 'url' => ['index']];
+$this->title = 'Обновить город: ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Города', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Обновить';
 ?>
 <div class="city-update">
 
