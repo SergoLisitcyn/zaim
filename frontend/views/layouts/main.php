b4460245127ffc2bf9081b04ebbe86f6750ad7b1
sergo <sergolisitcyn@gmail.com>
2021-08-17 08:48:26 +0200

    new

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -40,9 +40,9 @@ if($url == '/'){
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
     <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(37301585, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true, ecommerce:"dataLayer" }); </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/37301585" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!--    <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};-->
+<!--        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(37301585, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true, ecommerce:"dataLayer" }); </script>-->
+<!--    <noscript><div><img src="https://mc.yandex.ru/watch/37301585" style="position:absolute; left:-9999px;" alt="" /></div></noscript>-->
     <!-- /Yandex.Metrika counter -->
 </head>
 <body class="custom-background" id="<?= $class ?>">
