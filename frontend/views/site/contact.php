9b88ebe8ac19595fc648f6e6a1674cd5c841e5d0
sergo <sergolisitcyn@gmail.com>
2021-10-13 01:28:53 +0200

    mfo

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -33,63 +33,63 @@ $this->registerMetaTag(['name' => 'description',
                             Сұрақ қою үшін төмендегі нысанды қолданыңыз. Жақын арада жауап берейік.
                         </p>
                     </div>
-                    <div class="article-contacts">
-                        <div class="article-contacts__inner">
-                            <div class="feedback-form article-contacts__form">
-                                <div class="feedback-form__title">Кері байланыс нысаны</div>
-                                <form>
-                                    <div class="feedback-form__fields">
-                                        <div class="feedback-form__group feedback-form__group--sm">
-                                            <select name="question">
-                                                <option disabled="" selected="">Сұрақ санатын таңдаңыз</option>
-                                                <option>Қарызды алу</option>
-                                                <option>Қарызды өтеу</option>
-                                                <option>Ынтымақтастық ұсынысы</option>
-                                                <option>Өзге сұрақтар</option>
-                                            </select>
-                                        </div>
-                                        <div class="feedback-form__group feedback-form__group--sm">
-                                            <input type="text" name="surname" value="" size="40" placeholder="Тегі">
-                                        </div>
-                                        <div class="feedback-form__group feedback-form__group--sm">
-                                            <input type="text" name="name" value="" size="40" placeholder="Атыңыз">
-                                        </div>
-                                        <div class="feedback-form__group feedback-form__group--sm">
-                                            <input type="text" name="name" value="" size="40" placeholder="Әкесінің  аты">
-                                            <div class="control-check-muted">
-                                                <input type="checkbox" name="no-patronymic[]" value="Менің әкемнің аты жоқ">
-                                                <span>Менің әкемнің аты жоқ</span>
-                                            </div>
-                                        </div>
-                                        <div class="feedback-form__group feedback-form__group--sm">
-                                            <input type="email" name="mail" value="" size="40" placeholder="E-mail">
-                                        </div>
-                                        <div class="feedback-form__group feedback-form__group--sm">
-                                            <input type="tel" name="mobile" value="" size="40" class="tel" placeholder="+7 (___) ___-__-__">
-                                        </div>
-                                        <div class="feedback-form__group feedback-form__group--sm">
-                                            <input type="text" name="subject" value="" size="40" placeholder="Өтініш тақырыбы">
-                                        </div>
-                                        <div class="feedback-form__group">
-                                            <textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Сіз өз сұрағыңызды мүмкіндігінше егжей-тегжейлі сипаттаңыз"></textarea>
-                                        </div>
-                                        <div class="feedback-form__group feedback-form__group--submit">
-                                            <input type="submit" value="Жіберу" class="wpcf7-form-control wpcf7-submit"><br><br>
-                                            <span class="i_agree">«Жөнелту» батырмасын баса отырып, сіз <a class="i_agree-link" href="#" target="_blank">дербес деректерді өңдеуге</a> және SMS және Email арқылы жарнамалық хабарламалар алуға келісім бересіз.</span>
-                                        </div>
-                                    </div>
-                                </form>
-                                <div class="application_results_success">
-                                    <div class="credit-results-success">
-                                        <div class="credit-results-success__head">Спасибо за	обращение!
-                                        </div>
-                                        <div class="credit-results-success__text">Проверьте,	пожалуйста,	Вашу почту –	мы отправили	уведомление	о статусе	заявки.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<!--                    <div class="article-contacts">-->
+<!--                        <div class="article-contacts__inner">-->
+<!--                            <div class="feedback-form article-contacts__form">-->
+<!--                                <div class="feedback-form__title">Кері байланыс нысаны</div>-->
+<!--                                <form>-->
+<!--                                    <div class="feedback-form__fields">-->
+<!--                                        <div class="feedback-form__group feedback-form__group--sm">-->
+<!--                                            <select name="question">-->
+<!--                                                <option disabled="" selected="">Сұрақ санатын таңдаңыз</option>-->
+<!--                                                <option>Қарызды алу</option>-->
+<!--                                                <option>Қарызды өтеу</option>-->
+<!--                                                <option>Ынтымақтастық ұсынысы</option>-->
+<!--                                                <option>Өзге сұрақтар</option>-->
+<!--                                            </select>-->
+<!--                                        </div>-->
+<!--                                        <div class="feedback-form__group feedback-form__group--sm">-->
+<!--                                            <input type="text" name="surname" value="" size="40" placeholder="Тегі">-->
+<!--                                        </div>-->
+<!--                                        <div class="feedback-form__group feedback-form__group--sm">-->
+<!--                                            <input type="text" name="name" value="" size="40" placeholder="Атыңыз">-->
+<!--                                        </div>-->
+<!--                                        <div class="feedback-form__group feedback-form__group--sm">-->
+<!--                                            <input type="text" name="name" value="" size="40" placeholder="Әкесінің  аты">-->
+<!--                                            <div class="control-check-muted">-->
+<!--                                                <input type="checkbox" name="no-patronymic[]" value="Менің әкемнің аты жоқ">-->
+<!--                                                <span>Менің әкемнің аты жоқ</span>-->
+<!--                                            </div>-->
+<!--                                        </div>-->
+<!--                                        <div class="feedback-form__group feedback-form__group--sm">-->
+<!--                                            <input type="email" name="mail" value="" size="40" placeholder="E-mail">-->
+<!--                                        </div>-->
+<!--                                        <div class="feedback-form__group feedback-form__group--sm">-->
+<!--                                            <input type="tel" name="mobile" value="" size="40" class="tel" placeholder="+7 (___) ___-__-__">-->
+<!--                                        </div>-->
+<!--                                        <div class="feedback-form__group feedback-form__group--sm">-->
+<!--                                            <input type="text" name="subject" value="" size="40" placeholder="Өтініш тақырыбы">-->
+<!--                                        </div>-->
+<!--                                        <div class="feedback-form__group">-->
+<!--                                            <textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Сіз өз сұрағыңызды мүмкіндігінше егжей-тегжейлі сипаттаңыз"></textarea>-->
+<!--                                        </div>-->
+<!--                                        <div class="feedback-form__group feedback-form__group--submit">-->
+<!--                                            <input type="submit" value="Жіберу" class="wpcf7-form-control wpcf7-submit"><br><br>-->
+<!--                                            <span class="i_agree">«Жөнелту» батырмасын баса отырып, сіз <a class="i_agree-link" href="#" target="_blank">дербес деректерді өңдеуге</a> және SMS және Email арқылы жарнамалық хабарламалар алуға келісім бересіз.</span>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                </form>-->
+<!--                                <div class="application_results_success">-->
+<!--                                    <div class="credit-results-success">-->
+<!--                                        <div class="credit-results-success__head">Спасибо за	обращение!-->
+<!--                                        </div>-->
+<!--                                        <div class="credit-results-success__text">Проверьте,	пожалуйста,	Вашу почту –	мы отправили	уведомление	о статусе	заявки.-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
                 </div>
             </div>
             <?= \frontend\widgets\ContentSideBar::widget(); ?>
