9d7333f3b054e2c969e8c29c33ed851f0ba384b0
sergo <sergolisitcyn@gmail.com>
2021-11-15 20:17:51 +0200

    fix

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/main.css?v=49',
+        'css/main.css?v=50',
         'css/media.css?v=3',
         'css/fonts.css',
     ];
