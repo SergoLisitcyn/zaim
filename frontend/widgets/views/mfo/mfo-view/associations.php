58ea3ded121c67572ef5c636064c994fbcbab880
sergo <sergolisitcyn@gmail.com>
2022-06-21 00:07:55 +0200

    fix

--- frontend/widgets/views/mfo/mfo-view/associations.php
+++ frontend/widgets/views/mfo/mfo-view/associations.php
@@ -3,19 +3,19 @@
     <h2 class="mfo-about-title info-subtitle" style="padding-bottom: 20px;">Кәсіби бірлестіктерге мүшелік:</h2>
     <?php if($data['info']['amfok']) :  ?>
         <noindex>
-            <a href="//<?= $data['info']['amfok'] ?>" class="mfo-about__logos-link" rel="nofollow" target="_blank">
+            <a class="mfo-about__logos-link" rel="nofollow" target="_blank">
                 <noscript>
-                    <img src="/img/mfo/logo_amfok.png" alt="Halyk Bank">
+                    <img src="/img/mfo/logo_amfok.png" alt="Объединение юридических лиц «Ассоциация микрофинансовых организаций Казахстана»">
                 </noscript>
-                <img class="lazyloaded" src="/img/mfo/logo_amfok.png" data-src="/img/mfo/logo_amfok.png" alt="Ассоциация микрофинансовых организаций Казахстана">
+                <img class="lazyloaded" src="/img/mfo/logo_amfok.png" data-src="/img/mfo/logo_amfok.png" alt="Объединение юридических лиц «Ассоциация микрофинансовых организаций Казахстана»">
             </a>
         </noindex>
     <?php endif; ?>
     <?php if($data['info']['fintech']) :  ?>
         <noindex>
-            <a href="//<?= $data['info']['fintech'] ?>" class="mfo-about__logos-link" rel="nofollow" target="_blank">
+            <a class="mfo-about__logos-link" rel="nofollow" target="_blank">
                 <noscript>
-                    <img src="/img/mfo/logo_kazfin.png" alt="Halyk Bank">
+                    <img src="/img/mfo/logo_kazfin.png" alt="Объединение юридических лиц «Казахстанская Ассоциация ФинТех»">
                 </noscript>
                 <img class="lazyloaded" src="/img/mfo/logo_kazfin.png" data-src="/img/mfo/logo_kazfin.png" alt="Объединение юридических лиц «Казахстанская Ассоциация ФинТех»">
             </a>
