16a69521579f90b2fca895ed5c598a2e7070b824
sergo <sergolisitcyn@gmail.com>
2022-07-15 19:45:19 +0200

    fix

--- frontend/widgets/views/url.php
+++ frontend/widgets/views/url.php
@@ -1,5 +1,5 @@
 <?php if($output == 'desktop') { ?>
-<a class="desktop-visible" href="<?php echo $linkRu?>" style="display: inline-block;vertical-align: top;text-align: center;line-height: 30px;">RU</a>
+<a class="desktop-visible" hreflang="ru-kz" href="<?php echo $linkRu?>" style="display: inline-block;vertical-align: top;text-align: center;line-height: 30px;">RU</a>
 <?php } else { ?>
-<a href="<?php echo $linkRu?>" style="display: inline-block;color: #337ab7;vertical-align: top;text-align: center;line-height: 28px;font-size: 16px;">RU</a>
+<a href="<?php echo $linkRu?>"  hreflang="ru-kz" style="display: inline-block;color: #337ab7;vertical-align: top;text-align: center;line-height: 28px;font-size: 16px;">RU</a>
 <?php } ?>
