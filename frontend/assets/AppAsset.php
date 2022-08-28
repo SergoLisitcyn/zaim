6a0d46579f9158468b8be457493af85a018ee698
sergo <sergolisitcyn@gmail.com>
2022-08-28 11:52:23 +0200

    fix

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,8 +12,8 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/main.css?v=134',
-        'css/media.css?v=26',
+        'css/main.css?v=135',
+        'css/media.css?v=27',
         'css/fonts.css',
     ];
     public $js = [
