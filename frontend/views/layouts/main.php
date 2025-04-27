e556da8183797b61a6b1ba634bba1719c52a0565
Lisitsyn.SV <s.lisitsyn@roseltorg.ru>
2025-04-27 12:15:33 +0200

    test

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -39,14 +39,7 @@ if($url == '/'){
 </head>
 <body class="custom-background" id="<?= $class ?>">
 <?php $this->beginBody() ?>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2975264109383693" crossorigin="anonymous"></script>
-<!-- Homepage Leaderboard -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-2975264109383693"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<!--<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2975264109383693" crossorigin="anonymous"></script>-->
 <div class="wrapper">
     <header class="header">
         <div class="header-body">
